7f992561db96725ca860746a16000aabd3205d96
GabrielChiquetto <gabrielchiquetto@gmail.com>
2023-01-04 11:18:15 -0300

    Teste regra de lock de branch

--- EISOL_TestePraticoWebForms/Tarefa1.aspx.cs
+++ EISOL_TestePraticoWebForms/Tarefa1.aspx.cs
@@ -42,8 +42,7 @@ namespace EISOL_TestePraticoWebForms
         /// </summary>
         /// <param name="pessoa">DAO.PESSOAS</param>
         private void Gravar(DAO.PESSOAS pessoa)
-        {            
-            // Teste pull request
+        {                        
             // Se a pessoa for uma pessoa de verdade e feliz, com certeza ela será lembrada pelo banco de dados.
             new BLL.PESSOAS().Adicionar(pessoa);
             this.Alertar();
